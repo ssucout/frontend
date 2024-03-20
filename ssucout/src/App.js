52a94b4b662be818704090fd85ef54c2d55e9a5e
오채린 <chaerin@ochaelin-ui-MacBookAir.local>
2024-03-20 15:49:23 +0900

    3.20 main page

---
+++ ssucout/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <p>숭실대학교 동아리 찾기 서비스에 오신 것을 환영합니다</p>
+        <h1>SSU:Cout</h1>
+      </div>
+      <div>
+        <button>나에게 맞는 동아리 찾아보기</button>
+        <br />
+        <button>동아리 지원하러 가기</button>
+        <br />
+        <button>동아리방 둘러보기</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
