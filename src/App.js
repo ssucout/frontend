edadccc3505478f9794d34f69660cc3d8f5bb33f
오채린 <chaerin@ochaelin-ui-MacBookAir.local>
2024-04-01 15:56:15 +0900

    initial

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import MainPage from './components/MainPage.js'
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <p>숭실대학고 동아리 찾기 서비스에 오신 것을 환영합니다</p>
-        <h1>SSU:Cout</h1>
-      </div>
-      <div>
-        <button>나에게 맞는 동아리 찾아보기</button>
-        <br />
-        <button>동아리 지원하러 가기</button>
-        <br />
-        <button>동아리방 둘러보기</button>
-      </div>
+      <MainPage />
     </div>
   );
 }
