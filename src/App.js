4de038a63f0fc350d7b9ba0096e4eed85ebc9ac5
오채린 <chaerin@ochaelin-ui-MacBookAir.local>
2024-04-01 20:28:46 +0900

    QuizMainPage UI 완성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import MainPage from './components/MainPage.js'
+import QuizMainPage from './components/QuizMainPage.js';
 
 function App() {
   return (
     <div className="App">
       <MainPage />
+      <QuizMainPage />
     </div>
   );
 }
