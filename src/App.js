2501b9e5b27c8b956486d76055c967df6d51ee47
오채린 <chaerin@ochaelin-ui-MacBookAir.local>
2024-04-03 21:05:21 +0900

    Add Quiz1 Page. 폰트 수정 필요

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import './index.css'
 import MainPage from './components/MainPage.js'
-import QuizMainPage from './components/Quiz/QuizMainPage.js';
+// import QuizMainPage from './components/Quiz/QuizMainPage.js';
+import Quiz1 from './components/Quiz/Quiz1.js';
 
 function App() {
   return (
     <div className="App">
       <MainPage />
-      <QuizMainPage />
+      <Quiz1 />
     </div>
   );
 }
