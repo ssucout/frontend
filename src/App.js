4534d2bef98670a99a9364dc0b49d32854576351
WOOIN CHOI <choiwoo02in@gmail.com>
2024-04-04 15:06:07 +0900

    MainPage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import './index.css'
-import MainPage from './components/MainPage.js'
-// import QuizMainPage from './components/Quiz/QuizMainPage.js';
-import Quiz1 from './components/Quiz/Quiz1.js';
+//import MainPage from './components/MainPage.js'
+import QuizMainPage from './components/Quiz/QuizMainPage.js';
+//import Quiz1 from './components/Quiz/Quiz1.js';
 
 function App() {
   return (
     <div className="App">
-      <MainPage />
-      <Quiz1 />
+      <QuizMainPage />
     </div>
   );
 }
