45124a58484f12f87ebc73d8cb6f3dd0345bcb1e
WOOIN CHOI <choiwoo02in@gmail.com>
2024-05-06 00:55:55 +0900

    Quiz5_final

--- src/components/Quiz/Quiz5.js
+++ src/components/Quiz/Quiz5.js
@@ -10,9 +10,9 @@ function Quiz5() {
         <p className='QuizNum5'>5 / 10</p>
         <h2 className='QuizSentence5'>나는</h2>
         <div>
-        <button className='button5_1' onClick={() => navigate('/quiz/quiz6')}>앗 모르는 사람.... 먼저 말을 걸 때까지 기다린다</button>
+        <button className='button5_1' onClick={() => navigate('/quiz/quiz6')}>2D가 좋다.</button>
         <br />
-        <button className='button5_2' onClick={() => navigate('/quiz/quiz6')}>처음 만나는 동기...!! 먼저 인사를 건네본다</button>
+        <button className='button5_2' onClick={() => navigate('/quiz/quiz6')}>3D가 좋다.</button>
         </div>
     </div>
     
