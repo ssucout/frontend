f30dfe28ab358097e65c5eeb455e82730ef2435f
WOOIN CHOI <choiwoo02in@gmail.com>
2024-06-03 22:17:56 +0900

    메인모달 반응형 구현

--- src/components/MainModal.jsx
+++ src/components/MainModal.jsx
@@ -13,9 +13,11 @@ function MainModal({isOpen, closeModal}) {
   }
   return (
     <>
-    <div style={{ display: isOpen ? "block" : "none" }}>
-    {/* <BackgroundOverlay isOpen={isOpen} /> */}
-        <div>
+    
+    {/* <div style={{ display: isOpen ? "block" : "none" }}> */}
+    
+    <BackgroundOverlay isOpen={isOpen} onClick={closeModal} />
+    <ModalContainer isOpen={isOpen}>        
             <Box>
             <ButtonContainer>
             <ModalText>나에게 맞는 동아리 찾아보기</ModalText>
@@ -25,92 +27,154 @@ function MainModal({isOpen, closeModal}) {
 
             </ButtonContainer>
             </Box>
-        </div>
-    </div>
+            </ModalContainer>    
     </>
   );
 }
 
-// const BackgroundOverlay = styled.div`
-//   position: fixed;
-//   top: 0;
-//   left: 0;
-//   width: 100%;
-//   height: 100%;
-//   background-color: rgba(0, 0, 0, 0.5); /* 반투명한 검은색 배경 */
-//   z-index: 999; /* 모달보다 뒤에 위치하도록 설정 */
-//   display: ${({ isOpen }) => (isOpen ? 'block' : 'none')}; /* 모달이 열렸을 때만 표시 */
-// `;
+const BackgroundOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+  z-index: 999;
+  display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
+`;
 
+const ModalContainer = styled.div`
+  display: ${({ isOpen }) => (isOpen ? 'flex' : 'none')};
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1000;
+`;
 
 const Box = styled.div`
-  width: 53rem;
-  height: 38.8125rem;
+  width: 90vw;
+  height: 45vh;
   background-color: white;
   border-radius: 2.5rem;
-
-  position: absolute;
-  top: 40.5625rem;
-  transform: translate(-50%, -50%);
+  position: relative;
   display: flex;
-  left: 50%;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
+  @media (min-width: 768px) {
+    width: 85vw;
+    height: 55vh;
+  }
+
+  @media (min-width: 1200px) {
+    width: 53rem;
+    height: 70vh;
+  }
 `;
 
 const ModalText = styled.div`
   position: relative;
   font-family: "Pretendard-Bold";
-  font-size: 3rem;
-  top: 3.5rem;
-  color: #9A8CA3;
+  font-size: 1.5rem;
+  top: 2.5rem;
+  color: #9a8ca3;
+
+  @media (min-width: 768px) {
+    font-size: 2.5rem;
+  }
+
+  @media (min-width: 1200px) {
+    font-size: 3rem;
+    top: 3.5rem;
+  }
 `;
 
 const XButton = styled.button`
   position: relative;
-  top: -3.125rem;
-  right: -23rem;
+  top: -2rem;
+  right: -9rem;
   background-color: white;
-  color: #D8D8D8;
+  color: #d8d8d8;
   border: none;
-  font-size: 2rem;
+  font-size: 1rem;
+
+  @media (min-width: 768px) {
+    font-size: 1.75rem;
+    top: -3.125rem;
+    right: -23rem;
+  }
+
+  @media (min-width: 1200px) {
+    font-size: 2rem;
+    top: -3.125rem;
+    right: -23rem;
+  }
 `;
 
 const Button1 = styled.button`
   position: relative;
-  width: 42.6875rem;
-  height: 11.75rem;
+  width: 90vw;
+  height: 8rem;
   left: 50%;
   border-radius: 3.75rem;
   transform: translateX(-50%);
   background-color: white;
   color: #9b8ba3;
   border-color: #9b8ba3;
-  border-width: .4rem;
+  border-width: 0.4rem;
   font-family: "Pretendard-Bold";
-  font-size: 2.25rem;
+  font-size: 1.5rem;
   font-weight: bold;
-  top: 2rem;
+  top: 0rem;
   margin-bottom: 1rem;
+
+  @media (min-width: 768px) {
+    width: 80vw;
+    height: 9rem;
+    font-size: 2rem;
+    top: 2rem;
+  }
+
+  @media (min-width: 1200px) {
+    width: 42.6875rem;
+    height: 11.75rem;
+    font-size: 2.25rem;
+    top: 2rem;
+  }
 `;
 
 const Button2 = styled.button`
   position: relative;
-  width: 42.6875rem;
-  height: 11.75rem;
+  width: 90vw;
+  height: 8rem;
   left: 50%;
   border-radius: 3.75rem;
   transform: translateX(-50%);
   background-color: #9b8ba3;
   color: white;
   font-family: "Pretendard-Bold";
-  font-size: 2.25rem;
+  font-size: 1.5rem;
   font-weight: bold;
-  top: 2rem;
+  top: -5%;
   border: none;
-  /* padding-top : .625rem; */
-`;
 
+  @media (min-width: 768px) {
+    width: 80vw;
+    height: 9rem;
+    font-size: 2rem;
+    top: 2rem;
+  }
+
+  @media (min-width: 1200px) {
+    width: 42.6875rem;
+    height: 11.75rem;
+    font-size: 2.25rem;
+    top: 2rem;
+  }
+`;
 
 const ButtonContainer = styled.div`
   position: relative;
@@ -120,6 +184,6 @@ const ButtonContainer = styled.div`
   gap: 1rem;
   left: 50%;
   transform: translateX(-50%);
-`
+`;
 
-export default MainModal
+export default MainModal;
